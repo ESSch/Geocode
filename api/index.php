9f322c14ed20ca04ca5eeec41ae250c05bb25ccb
ESSch <ESSch@yandex.ru>
2018-08-27 05:29:52 +0300

    microservices

--- api/index.php
+++ api/index.php
@@ -4,22 +4,6 @@ ob_start;
 try {
     include_once __DIR__ . '/lib.php';
 
-    /**
-     * @expamle curl -XPUT 'http://geocode1.essch.ru:9200/app/log/1?pretty' -d '{"url": "https://habr.com/post/280488/"}'
-                curl -XGET 'http://geocode1.essch.ru:9200/app/log'
-     */
-//     echo post("/app5", [
-//        "mappings" => [
-//            "log" => [
-//                "properties" => [
-//                    "date" => [
-//                        "type" => "date",
-//                    ]
-//                ]
-//            ]
-//        ]
-//     ]);
-
     // show http://geocode1.essch.ru:9200/app5/log/_search?pretty
     $list = post("/app5/log/" .  mktime(), [
         "query" => $_GET["query"],
